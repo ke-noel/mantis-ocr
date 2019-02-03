3c241d0a263e02ba0d186a858886dcaf8dd8f19d
Kira <kira.e.noel@gmail.com>
2019-02-03 00:05:01 -0500

    Kira needs sleep soon

--- eventPage.js
+++ eventPage.js
@@ -9,7 +9,6 @@ if (!chrome.contextMenus["describeImageText"]) {
 chrome.contextMenus.onClicked.addListener(function(request){
 	if (request.menuItemId == "describeImageText" && request.srcUrl) {
 		var imgSrc = request.srcUrl;
-		//alert('success! Url: ' + imgSrc);  // for testing
 		processText(imgSrc);
 	}
 })
